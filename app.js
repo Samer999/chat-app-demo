e7c8a0cfb055b1e8abf52e8a86f2d5f33773b060
Samer999 <59462698+Samer999@users.noreply.github.com>
2021-05-27 10:32:00 +0300

    use process.env.PORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const io = require('socket.io')(server, {
         credentials: true
     }
 });
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 const uri = 'mongodb+srv://admin:123@cluster0.ookg1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
